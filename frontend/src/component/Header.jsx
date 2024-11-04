b347cb6506326ca454ee8fa7964dbde8d6a065bc
TranThanhTung21099571 <thanhtung2003nt@gmail.com>
2024-11-04 21:40:34 +0700

    upload category

--- frontend/src/component/Header.jsx
+++ frontend/src/component/Header.jsx
@@ -1,81 +1,74 @@
-import React from 'react'
-import { Box, Typography, TextField, InputBase } from '@mui/material'
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import SearchIcon from '@mui/icons-material/Search';
-import shop_logo from '../assets/Shop_logo.png'
+import React from "react";
+import { Box, Typography, TextField, InputBase } from "@mui/material";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import SearchIcon from "@mui/icons-material/Search";
+import shop_logo from "../assets/Shop_logo.png";
 const Header = () => {
   return (
     <Box
-        sx={{
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'space-between'
-        }}
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+      }}
     >
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <img src={shop_logo} alt="" style={{ width: 100, height: 100 }} />
+        <Typography variant="h4" fontWeight={700}>
+          PhoneGO
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          border: "1px solid black",
+          width: "600px",
+          pl: 2,
+          borderRadius: 2,
+          borderColor: "#3E81FF",
+        }}
+      >
+        <InputBase
+          placeholder="Tìm Kiếm..."
+          size="small"
+          sx={{ width: "90%", mt: "4px" }}
+        />
+        <SearchIcon sx={{ ml: 2 }} />
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+        }}
+      >
         <Box
-            sx={{
-                display: 'flex',
-                alignItems: 'center'
-            }}
+          sx={{
+            display: "flex",
+            mr: 6,
+          }}
         >
-            <img src={shop_logo} alt="" style={{width: 100, height: 100}}/>
-            <Typography
-                variant='h4'
-                fontWeight={700}
-            >
-                PhoneGO
-            </Typography>
+          <AccountCircleIcon />
+          <Typography variant="body1">Tài khoản</Typography>
         </Box>
         <Box
-            sx={{
-                display: 'flex',
-                alignItems: 'center',
-                border: '1px solid black',
-                width: '600px',
-                pl: 2,
-                borderRadius: 2,
-                borderColor: '#3E81FF'
-            }}
+          sx={{
+            display: "flex",
+            mr: 6,
+          }}
         >
-            <InputBase placeholder='Tìm Kiếm...' size='small' sx={{width: '90%', mt: '4px'}}/>
-            <SearchIcon sx={{ml: 2}}/>
+          <ShoppingCartIcon />
+          <Typography variant="body1">Tài khoản</Typography>
         </Box>
-        <Box
-            sx={{
-                display: 'flex'
-            }}
-        >
-            <Box
-                sx={{
-                    display: 'flex',
-                    mr: 6
-                }}
-            >
-                <AccountCircleIcon />
-                <Typography
-                    variant='body1'
-                >
-                    Tài khoản
-                </Typography>
-            </Box>
-            <Box
-                sx={{
-                    display: 'flex',
-                    mr: 6
-                }}
-            >
-                <ShoppingCartIcon />
-                <Typography
-                    variant='body1'
-                >
-                    Tài khoản
-                </Typography>
-            </Box>
-        </Box>
+      </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
